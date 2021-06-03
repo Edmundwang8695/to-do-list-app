72b3037dad761e70d4468fa06940ce556919270d
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-06-02 23:14:44 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function newItem(){
+    let li = $('<li></li>');
+    let inputValue = $('input').val;
+    li.append(inputValue);
+
+    if(inputValue === ''){
+        alert('You must enter something!');
+    }else {
+        let list = $('#list').append(li)
+    }
+
+function crossOut() {
+    li.toggleClass('strike');
+      }
+    li.on(dbclick, function crossOut(){
+    li.crossOut('strike');
+});
+
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+crossOutButton.on(click, deleteListItem)
+
+function deleteListItem(){
+    li.addClass('delete')
+}
+$('#list').sortable();
+}
